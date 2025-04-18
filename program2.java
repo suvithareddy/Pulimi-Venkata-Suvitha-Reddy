068056684ae00f7c2a2ac0337ead39dc6039db89
suvithareddy <100707040+suvithareddy@users.noreply.github.com>
2025-04-18 17:03:17 +0530

    Add files via upload

---
+++ program2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a: ");
+        int a = sc.nextInt();
+
+        for (int i = 0; i < a; i++) {
+            System.out.print((2 * i + 1));
+            if (i < a - 1) System.out.print(", ");
+        }
+
+        sc.close();
+    }
+}
